560f4f22efe35cf329dba6b70b4d898b5eca6533
Rawkins21 <rawkins1111@gmail.com>
2022-11-08 14:41:37 -0300

    edit

---
+++ controllers/VendedorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController{
+    public static function crear(){
+        echo "crear vendedor";
+    }
+
+    public static function actualizar(){
+        echo "actualizar vendedor";
+    }
+
+    public static function borrar(){
+        echo "borrar vendedor";
+    }
+}
