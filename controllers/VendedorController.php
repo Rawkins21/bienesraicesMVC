ab4eb0eb3b19f0654e35acb63413bd215c6af8e2
Rawkins21 <rawkins1111@gmail.com>
2022-11-16 13:51:07 -0300

    edit

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -10,6 +10,40 @@ class VendedorController{
 
         $errores = Vendedor::getErrores();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+            $vendedor = new Vendedor($_POST['vendedor']);
+        
+             // Generar un nombre unico y su extension
+             $nombreImagen = md5( uniqid(rand(), true)) .  ".jpg";
+        
+             // Setear la imagen
+             // Realiza un resize a la imagen con InterventionImage
+             if($_FILES['vendedor']['tmp_name']['imagen']){
+                 $image = Image::make($_FILES['vendedor']['tmp_name']['imagen'])->fit(800,600);
+                 $vendedor->setImagen($nombreImagen);
+             }
+             
+               // Validar
+            $errores = $vendedor->validar();
+         
+             if(empty($errores)){
+        
+             
+         
+               // Crea la carpeta para subir imagenes en caso de que no exista y verifica su existencia para no volver a crearla
+               if(!is_dir(CARPETA_IMAGENES)){
+                   mkdir(CARPETA_IMAGENES);
+               }
+         
+             //Guarda al imagen en el servidor
+             $image->save(CARPETA_IMAGENES . $nombreImagen);
+         
+               
+             // Guarda en la base de datos
+             $vendedor->crear();
+        }
+
         $vendedor = new Vendedor;
 
         $router-> render('vendedores/crear',[
