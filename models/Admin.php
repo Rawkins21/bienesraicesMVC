b6a4c25d96d8fab7d4d74dce630a273cd0dcb4ce
Rawkins21 <rawkins1111@gmail.com>
2022-11-30 19:39:52 -0300

    edit

--- models/Admin.php
+++ models/Admin.php
@@ -10,4 +10,12 @@ class Admin extends ActiveRecord{
     public $id;
     public $email;
     public $password;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args ['id'] ?? null;
+        $this->email = $args ['email'] ?? '';
+        $this->password = $args ['password'] ?? '';
+
+    }
 }
