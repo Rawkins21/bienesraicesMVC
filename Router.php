24bcbf1360825987a1468dfc6d24f56c4081866d
Rawkins21 <rawkins1111@gmail.com>
2022-12-02 20:42:26 -0300

    done

--- Router.php
+++ Router.php
@@ -16,6 +16,15 @@ class Router{
     }
 
     public function comprobarRutas(){
+
+        session_start();
+
+        $auth = $_SESSION['login'] ?? null;
+
+        // Arreglo de rutas protegidas
+        $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar' ];
+
+
         $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -25,6 +34,12 @@ class Router{
         } else{
             $fn = $this->rutasPOST[$urlActual] ?? null;
         }
+
+        // Proteger las rutas
+        if(in_array($urlActual, $rutas_protegidas) &&!$auth){
+            header('Location: /public');
+        }
+
         if($fn){
         call_user_func($fn, $this); //Permite llamar a una funcion cuando no sabemos el nombre
 
